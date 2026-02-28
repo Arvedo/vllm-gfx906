87b19d43333326a145ecfbcba546215554e1951e
Arved <arved_oberstrass@teilnehmer.cdemy.de>
2026-02-28 20:59:27 +0100

    Refactor ignore_keys_at_rope_validation to use a set in Qwen3_5TextConfig and Qwen3_5MoeTextConfig classes; add tests for configuration entries and validation keys.

--- tests/transformers_utils/test_qwen35_config_registry.py
+++ tests/transformers_utils/test_qwen35_config_registry.py
@@ -3,6 +3,8 @@
 
 from vllm.transformers_utils import configs
 from vllm.transformers_utils.config import _CONFIG_REGISTRY
+from vllm.transformers_utils.configs.qwen3_5 import Qwen3_5TextConfig
+from vllm.transformers_utils.configs.qwen3_5_moe import Qwen3_5MoeTextConfig
 
 
 def test_qwen35_config_registry_entries():
@@ -15,3 +17,11 @@ def test_qwen35_configs_exported():
     assert hasattr(configs, "Qwen3_5TextConfig")
     assert hasattr(configs, "Qwen3_5MoeConfig")
     assert hasattr(configs, "Qwen3_5MoeTextConfig")
+
+
+def test_qwen35_rope_ignore_keys_are_set():
+    qwen35_text_config = Qwen3_5TextConfig()
+    qwen35_moe_text_config = Qwen3_5MoeTextConfig()
+
+    assert isinstance(qwen35_text_config.ignore_keys_at_rope_validation, set)
+    assert isinstance(qwen35_moe_text_config.ignore_keys_at_rope_validation, set)
